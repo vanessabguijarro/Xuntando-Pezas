b132d9cb30a8485e72cc2c5e31671e80d7729707
CasafutbolGit <powebicreacion@gmail.com>
2025-04-24 23:31:19 +0200

    Update Formulario.tsx

--- FRONT/Front-Xuntando-Pezas/src/PAXINAS/PUBLICAS/Formulario.tsx
+++ FRONT/Front-Xuntando-Pezas/src/PAXINAS/PUBLICAS/Formulario.tsx
@@ -1,1 +1,21 @@
+const handleSubmit = async (e) => {
+e.preventDefault();
 
+const usuario = { nombre , correo , contraseña } ;
+
+  try {
+
+  const response = await fetch ('http://localhost:3001/api/usuarios', {
+    method: 'POST',
+    headers: {
+      'Content-Type: 'application/json',
+    },
+    body: JSON.stringfi(usuario),
+  });
+
+  const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error al crear el usuario:', error);
+  }
+};
