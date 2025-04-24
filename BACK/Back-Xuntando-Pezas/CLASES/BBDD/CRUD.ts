4cc1f6e4510238da869b5904157685b2a1eb97b9
Israel <cursos.israelsp@gmail.com>
2025-04-24 17:05:35 +0200

    Actualizando accesoUser

--- BACK/Back-Xuntando-Pezas/CLASES/BBDD/CRUD.ts
+++ BACK/Back-Xuntando-Pezas/CLASES/BBDD/CRUD.ts
@@ -1,4 +1,5 @@
 import { Database } from "sqlite";
+import { datoUser } from "../../Tipos/bbdd.tipos";
 
 export class CRUD{
     
@@ -9,7 +10,7 @@ export class CRUD{
         this.conexion = conexion;
     }
 
-    async insertar(sentenciaSQL:String){
+    async insertar(sentenciaSQL:string){
         try{
             (await this.conexion).run(`${sentenciaSQL}`)
         }catch(e){
@@ -17,9 +18,10 @@ export class CRUD{
         }
     }
 
-    async lerUnhaFila(sentenciaSQL:String):Promise<Object>{
+    async lerUnhaFila(sentenciaSQL:string,valorCampo:string):Promise<datoUser>{
         try{
-            let dato = (await this.conexion).get(`${sentenciaSQL}`)
+            //let dato = (await this.conexion).get(`${sentenciaSQL}`)
+            let dato = (await this.conexion).get(`${sentenciaSQL}`,`${valorCampo}`)
             console.log("dato en funcion ler ",await dato)
             return await dato
         }catch(e){
@@ -28,7 +30,7 @@ export class CRUD{
         
     }
 
-    async lerTodasAsFilas(sentenciaSQL:String):Promise<Object>{
+    async lerTodasAsFilas(sentenciaSQL:string):Promise<Object>{
         try{
             let datos = (await this.conexion).all(`${sentenciaSQL}`)
             return await datos
--- BACK/Back-Xuntando-Pezas/CONTROLADORES/USERS/accesoUser.ts
+++ BACK/Back-Xuntando-Pezas/CONTROLADORES/USERS/accesoUser.ts
@@ -1,27 +1,35 @@
 import { Request, Response } from "express";
 import Jwt from "jsonwebtoken";
+import { execucionTodoBBDD } from "../../instruccions.base.sqlite";
+import { listaInstruccions } from "../../datos/lista.instruccions.bbdd.israel";
 
-export const accesoUser = (req:Request,res:Response)=>{
+import { datoUser } from "../../Tipos/bbdd.tipos";
 
+export const accesoUser = async (req:Request,res:Response)=>{
     // DESESTRUCTURACION CON TYPESCRIPT
     //const { username } : {username : string} = req.body
     const { username } = req.body
-    console.log("req.body ",req.body,username,typeof req.body,typeof username)
+    
+     try {
 
-    /* try {
-        if(username === 'Israel' ){
-            const token = jwt.sign({ user: username }, process.env.SEGREDO || "clavePorDefecto");
+        let datoUserLido : datoUser;// mirar con 'const' xq non funciona
+        let instanciaBBDD = execucionTodoBBDD()
+        
+        //datoUserLido = await instanciaBBDD.lerUnhaFila(`select name_user_traballador,pwd_traballador from TRABALLADOR where name_user_traballador = ?`,'Israel')
+        datoUserLido = await instanciaBBDD.lerUnhaFila(listaInstruccions.instruccion.sqlLecturaUser,username)
+        
+        // Lemos o resultado na base de datos
+        
+        if(username === datoUserLido.NAME_USER_TRABALLADOR ){
+            const token = Jwt.sign({ user: username }, process.env.SEGREDO || "clavePorDefecto");
+            console.log(typeof process.env.SEGREDO)
             res.json({ token });
         }
     } catch (error) {
         console.error("Error al firmar el token:", error);
         res.status(500).send("Error interno del servidor");
-    } */
-     if(username === 'Israel' ){
-        const token = Jwt.sign({ user: username }, process.env.SEGREDO || "clavePorDefecto");
-        console.log(typeof process.env.SEGREDO)
-        res.json({ token });
     } 
+      
 
 
 }
