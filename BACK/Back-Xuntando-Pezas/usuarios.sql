b2ce4dc203d0ea21c3cb797cf40c88c09ef0bcb7
Fran <powebicreacion@gmail.com>
2025-05-05 19:00:20 +0200

    users.sql

--- BACK/Back-Xuntando-Pezas/usuarios.sql
+++ BACK/Back-Xuntando-Pezas/usuarios.sql
@@ -1,37 +1,36 @@
+-- Tabla USERS: Información básica de los usuarios
 CREATE TABLE IF NOT EXISTS USERS (
-    ID_USER                          INTEGER PRIMARY KEY AUTOINCREMENT,
-    NAME                             CHAR(50),
-    APELIDO1                         CHAR(50),
-    APELIDO2                         CHAR(50),
-    PWD                              CHAR(50),
-    EMAIL                            CHAR(50)
+    ID_USER      INTEGER PRIMARY KEY AUTOINCREMENT,
+    NAME         VARCHAR(50),
+    APELIDO1     VARCHAR(50),
+    APELIDO2     VARCHAR(50),
+    PWD          VARCHAR(50),
+    EMAIL        VARCHAR(50) UNIQUE -- Relacionado con la tabla EMAIL
 );
 
+-- Tabla EMAIL: Correos adicionales o detalles relacionados
 CREATE TABLE IF NOT EXISTS EMAIL (
-    ID                              INTEGER PRIMARY KEY AUTOINCREMENT,
-    NAME                            CHAR(50),
-    APELIDO1                        CHAR(50),
-    EMPRESA                         TEXT NOT NULL,
-    PROFESION                       TEXT NULL,
-    ROL                             TEXT NULL,
-    EMAIL                           CHAR(50)
+    ID_EMAIL     INTEGER PRIMARY KEY AUTOINCREMENT,
+    ID_USER      INTEGER NOT NULL,  -- Relación con USERS
+    EMAIL        VARCHAR(50) UNIQUE,
+    FOREIGN KEY (ID_USER) REFERENCES USERS (ID_USER)
 );
 
+-- Tabla STATUS: Estado de los clientes
 CREATE TABLE IF NOT EXISTS STATUS (
-    ID                              INTEGER PRIMARY KEY AUTOINCREMENT,
-    ID_CLIENTE                     INTEGER NOT NULL,
-    TOTAL                          REAL,
-    PAGADA                         INTEGER,
+    ID           INTEGER PRIMARY KEY AUTOINCREMENT,
+    ID_CLIENTE   INTEGER NOT NULL,  -- Relación con USERS
+    TOTAL        REAL,
+    PAGADA       INTEGER,
     FOREIGN KEY (ID_CLIENTE) REFERENCES USERS (ID_USER)
 );
 
+-- Tabla ROLE: Roles de los usuarios
 CREATE TABLE IF NOT EXISTS ROLE (
-    ID                              INTEGER PRIMARY KEY AUTOINCREMENT,
-    NAME                            CHAR(50),
-    EMPRESA                         TEXT NOT NULL,
-    PROFESION                       TEXT NULL,
-    ROL                             TEXT NULL,
-    EMAIL                           CHAR(50)
+    ID_ROLE      INTEGER PRIMARY KEY AUTOINCREMENT,
+    ID_USER      INTEGER NOT NULL,  -- Relación con USERS
+    ROLE         VARCHAR(50),
+    FOREIGN KEY (ID_USER) REFERENCES USERS (ID_USER)
 );
 
 
