00f7bfcc4b56b145fb8fcbb25127a807501a029c
Fran <powebicreacion@gmail.com>
2025-05-05 18:11:27 +0200

    usuarios

--- BACK/Back-Xuntando-Pezas/usuarios.sql
+++ BACK/Back-Xuntando-Pezas/usuarios.sql
@@ -1,30 +1,30 @@
 CREATE TABLE if not exists NAME(
     ID_USER_NAME            INTEGER PRIMARY KEY AUTOINCREMENT,
-    NAME                    VARCHAR(50),
-    APELIDO1                VARCHAR(50),
-    APELIDO2                VARCHAR(50),
-    PWD                     VARCHAR(50),
-    EMAIL                   VARCHAR(50)
+    NAME_NAME                    VARCHAR(50),
+    APELIDO1_NAME                VARCHAR(50),
+    APELIDO2_NAME                VARCHAR(50),
+    PWD_NAME                     VARCHAR(50),
+    EMAIL_NAME                   VARCHAR(50)
 );
 
 CREATE TABLE if not exists TRABALLADOR (
     ID_TRABALLADOR          INTEGER PRIMARY KEY AUTOINCREMENT,
-    NAME                    VARCHAR(50),
-    APELIDO1                VARCHAR(50),
-    EMPRESA                 TEXT(200) NOT NULL,
-    PROFESION               TEXT(200) NULL,
-    ROL                     TEXT(200) NULL,
-    EMAIL                   VARCHAR(50)
+    NAME_TRABALLADOR                    VARCHAR(50),
+    APELIDO1_TRABALLADOR                VARCHAR(50),
+    EMPRESA_TRABALLADOR                 TEXT(200) NOT NULL,
+    PROFESION_TRABALLADOR               TEXT(200) NULL,
+    ROL_TRABALLADOR                     TEXT(200) NULL,
+    EMAIL-TRABALLADOR                   VARCHAR(50)
 );
 
 CREATE TABLE if not exists EMAIL(
     ID_EMAIL                INTEGER PRIMARY KEY AUTOINCREMENT,
     CODIGO_EMAIL            VARCHAR(50) UNIQUE,
-    NOME_PRODUCTO           VARCHAR(50),
-    FECHA                   VARCHAR(50),
-    CODIGO_PRODUCTO_EMAIL   VARCHAR(50),
-    CANTIDADE_PRODUCTO      INTEGER,
-    PRECIO_PRODUCTO         REAL,
+    NOME_EMAIL           VARCHAR(50),
+    FECHA_EMAIL                   VARCHAR(50),
+    CODIGO__PROCUCTO_EMAIL   VARCHAR(50),
+    CANTIDADE_EMAIL      INTEGER,
+    PRECIO_EMAIL         REAL,
     FOREIGN KEY (CODIGO_PRODUCTO_EMAIL) REFERENCES PRODUCTO (CODIGO_PRODUCTO) ON DELETE SET NULL
 );
 
@@ -32,8 +32,8 @@ CREATE TABLE if not exists STATUS(
     ID_STATUS               INTEGER PRIMARY KEY AUTOINCREMENT,
     ID_CLIENTE_STATUS       INTEGER NOT NULL,
     CODIGO_FACTURA_STATUS   VARCHAR(50),
-    TOTAL                   REAL,
-    PAGADA                  INTEGER,
+    TOTAL_STATUS                   REAL,
+    PAGADA_STATUS                  INTEGER,
     FOREIGN KEY (CODIGO_FACTURA_STATUS) REFERENCES FACTURA (CODIGO_FACTURA) ON UPDATE CASCADE,
     FOREIGN KEY (ID_CLIENTE_STATUS) REFERENCES CLIENTE (ID_CLIENTE)
 );
