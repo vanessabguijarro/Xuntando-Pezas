b4def4b9aa451de8052da15c518f4e6c983cec01
Fran <powebicreacion@gmail.com>
2025-05-05 18:40:07 +0200

    quitando redundancias en sql

--- BACK/Back-Xuntando-Pezas/usuarios.sql
+++ BACK/Back-Xuntando-Pezas/usuarios.sql
@@ -1,44 +1,46 @@
 CREATE TABLE IF NOT EXISTS USERS (
-    ID_USER_NAME                     INTEGER PRIMARY KEY AUTOINCREMENT,
-    NAME_USER_NAME                   CHAR(50),
-    APELIDO1_NAME                    CHAR(50),
-    APELIDO2_NAME                    CHAR(50),
-    PWD_NAME                         CHAR(50),
-    EMAIL_NAME                       CHAR(50)
+    ID_USER                          INTEGER PRIMARY KEY AUTOINCREMENT,
+    NAME                             CHAR(50),
+    APELIDO1                         CHAR(50),
+    APELIDO2                         CHAR(50),
+    PWD                              CHAR(50),
+    EMAIL                            CHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS TRABALLADOR (
-    ID_TRABALLADOR                     INTEGER PRIMARY KEY AUTOINCREMENT,
-    NAME_TRABALLADOR                   CHAR(50),
-    APELIDO1_TRABALLADOR               CHAR(50),
-    EMPRESA_TRABALLADOR                TEXT NOT NULL,
-    PROFESION_TRABALLADOR              TEXT NULL,
-    ROL_TRABALLADOR                    TEXT NULL,
-    EMAIL_TRABALLADOR                  CHAR(50)
+    ID                              INTEGER PRIMARY KEY AUTOINCREMENT,
+    NAME                            CHAR(50),
+    APELIDO1                        CHAR(50),
+    EMPRESA                         TEXT NOT NULL,
+    PROFESION                       TEXT NULL,
+    ROL                             TEXT NULL,
+    EMAIL                           CHAR(50)
 );
 
-CREATE TABLE IF NOT EXISTS EMAIL (
-    ID_EMAIL                            INTEGER PRIMARY KEY AUTOINCREMENT,
-    CODIGO_EMAIL                        CHAR(50),
-    NOME_PRODUCTO_EMAIL                 CHAR(50),
-    FECHA_EMAIL                         CHAR(50),
-    CANTIDADE_PRODUCTO_EMAIL            INTEGER,
-    PRECIO_PRODUCTO_EMAIL               REAL
+CREATE TABLE IF NOT EXISTS PEDIDO (
+    ID_PEDIDO                       INTEGER PRIMARY KEY AUTOINCREMENT,
+    CODIGO                          CHAR(50),
+    NOMBRE_PRODUCTO                 CHAR(50),
+    FECHA                           CHAR(50),
+    CANTIDAD                        INTEGER,
+    PRECIO                          REAL
 );
 
 CREATE TABLE IF NOT EXISTS STATUS (
-    ID_STATUS                          INTEGER PRIMARY KEY AUTOINCREMENT,
-    ID_CLIENTE_STATUS                  INTEGER NOT NULL,
-    TOTAL_STATUS                       REAL,
-    PAGADA_STATUS                      INTEGER,
-    FOREIGN KEY (ID_CLIENTE_STATUS) REFERENCES USERS (ID_USER_NAME)
+    ID                              INTEGER PRIMARY KEY AUTOINCREMENT,
+    ID_CLIENTE                     INTEGER NOT NULL,
+    TOTAL                          REAL,
+    PAGADA                         INTEGER,
+    FOREIGN KEY (ID_CLIENTE) REFERENCES USERS (ID_USER)
 );
 
 CREATE TABLE IF NOT EXISTS ROLE (
-    ID_ROLE                      INTEGER PRIMARY KEY AUTOINCREMENT,
-    NAME_ROLE                    CHAR(50),
-    EMPRESA_ROLE                 TEXT NOT NULL,
-    PROFESION_ROLE               TEXT NULL,
-    ROL_ROLE                     TEXT NULL,
-    EMAIL_ROLE                   CHAR(50)
+    ID                              INTEGER PRIMARY KEY AUTOINCREMENT,
+    NAME                            CHAR(50),
+    EMPRESA                         TEXT NOT NULL,
+    PROFESION                       TEXT NULL,
+    ROL                             TEXT NULL,
+    EMAIL                           CHAR(50)
 );
+
+
--- BACK/Back-Xuntando-Pezas/usuarios2.sql
+++ BACK/Back-Xuntando-Pezas/usuarios2.sql
@@ -44,3 +44,4 @@ CREATE TABLE IF NOT EXISTS ROLE (
 );
 
 
+
