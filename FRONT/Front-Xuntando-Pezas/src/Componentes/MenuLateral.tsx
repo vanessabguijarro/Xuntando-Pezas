79fe32774bb7649ec2d23dd182262fb23ca69b9e
Israel <cursos.israelsp@gmail.com>
2025-05-06 20:52:51 +0200

    Cambio de tamaño de MenuLateral

--- FRONT/Front-Xuntando-Pezas/src/Componentes/MenuLateral.tsx
+++ FRONT/Front-Xuntando-Pezas/src/Componentes/MenuLateral.tsx
@@ -12,32 +12,32 @@ export default function MenuLateral({url}:{url:number}){
     <nav className="menu-lateral">
         <Link className="link-menu-lateral" to="/app">
             <Icono imaxeUser={Imaxes.iconoHome} estilo="icono-menu-lateral"/>
-            <span style={{color:"white", width: "75px", fontSize:"10px"}}>Home</span>
+            <span style={{color:"white", width: "75px", fontSize:"15px"}}>Home</span>
         </Link>
                 {url === 2 ? "" : <Link className="link-menu-lateral" to="/app/invoices">
                     <Icono imaxeUser={Imaxes.iconoFacturas} estilo="icono-menu-lateral" />
-                    <span style={{color:"white", width: "75px", fontSize:"10px",fontWeight: "100"}}>Facturas</span>
+                    <span style={{color:"white", width: "75px", fontSize:"15px",fontWeight: "100"}}>Facturas</span>
             
                     
                 </Link>}
                 {
                 url === 1 ? "": <Link className="link-menu-lateral" to="/app/customers">
                 <Icono imaxeUser={Imaxes.iconoClientes} estilo="icono-menu-lateral" />
-                <span style={{color:"white", width: "75px", fontSize:"10px",fontWeight: "100"}}>Clientes</span>
+                <span style={{color:"white", width: "75px", fontSize:"15px",fontWeight: "100"}}>Clientes</span>
                 
                 </Link>}
                 {
                     url === 4 ? "" : <Link className="link-menu-lateral" to="/app/products">
                
                     <Icono imaxeUser={Imaxes.iconoProductos} estilo="icono-menu-lateral" />
-                    <span style={{color:"white", width: "75px", fontSize:"10px",fontWeight: "100"}}>Productos</span>
+                    <span style={{color:"white", width: "75px", fontSize:"15px",fontWeight: "100"}}>Productos</span>
                    
                     </Link>
                 }
                 {url === 3 ? "":
                 <Link className="link-menu-lateral" to="/app/users">
                 <Icono imaxeUser={Imaxes.iconoUsuarios} estilo="icono-menu-lateral" />
-                <span style={{color:"white", width: "75px", fontSize:"10px",fontWeight: "100"}}>Usuarios</span>
+                <span style={{color:"white", width: "75px", fontSize:"15px",fontWeight: "100"}}>Usuarios</span>
                
                 </Link>}
                 {/* <Boton estilo="" texto="Sair" funcion={logout}/> */}
