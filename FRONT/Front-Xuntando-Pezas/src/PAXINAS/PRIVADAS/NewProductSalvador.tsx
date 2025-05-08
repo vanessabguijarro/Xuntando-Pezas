4f49da838a32c5211d489d07196f9377bacbdc1c
Israel <cursos.israelsp@gmail.com>
2025-05-08 20:33:12 +0200

    Paxina Salvador

---
+++ FRONT/Front-Xuntando-Pezas/src/PAXINAS/PRIVADAS/NewProductSalvador.tsx
@@ -0,0 +1,9 @@
+// @ts-ignore
+import React from 'react';
+import NewProductTemplate from '../../Components/templates/NewProductTemplate';
+
+const NewProduct: React.FC = () => {
+  return React.createElement(NewProductTemplate, null);
+};
+
+export default NewProduct;
