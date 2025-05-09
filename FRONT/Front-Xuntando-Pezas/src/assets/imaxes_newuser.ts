792afa05e79b4efc92c3643ca72d53361fa46eb9
Israel <cursos.israelsp@gmail.com>
2025-05-09 16:40:02 +0200

    Gardo Adrian

---
+++ FRONT/Front-Xuntando-Pezas/src/assets/imaxes_newuser.ts
@@ -0,0 +1,17 @@
+import engranaxe from "./Engranaxe.png";
+import avatar from "./Avatar.png";
+import home from "./Home.png";
+import persoas from "./Persoas.png";
+import profile from "./Profile.png";
+import rectangle from "./Rectangle.png";
+import shape from "./Shape.png";
+
+export const Imaxes = {
+  engranaxe,
+  avatar,
+  home,
+  persoas,
+  profile,
+  rectangle,
+  shape,
+};
